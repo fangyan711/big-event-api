a7317cc583ee586501131c9669734b8d70cec0df
byte0 <bbcde_1234@163.com>
2020-07-01 15:27:31 +0800

    获取用户的token信息

--- routers/user.js
+++ routers/user.js
@@ -8,6 +8,12 @@ const router = express.Router()
 
 // 获取用户信息
 router.get('/userinfo', (req, res) => {
+  // 根据什么获取用户的详细信息？用户的id
+  // 用户的id从哪里得到？token
+  // req.user表示从token中获取的信息，该信息是登录成功后放入的
+  // req.user属性名称是固定的吗？是的
+  // { username: 'tom', id: 8, iat: 1593587638, exp: 1593591238 }
+  console.log(req.user)
   res.send('userinfo')
 })
 
